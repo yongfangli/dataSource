04aa046784f3b7856aa984825c1bac80e001d495
liyongfang <effective_java@outlook.com>
2017-07-13 17:05:39 +0800

    Please enter the commit message for your changes. Lines starting

---
+++ mybatis-dao/src/main/java/com/meike/datasource/App.java
@@ -0,0 +1,13 @@
+package com.meike.datasource;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
